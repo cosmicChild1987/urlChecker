ae6ea1c78696c3b6448cc43f3eeabffe29217edd
Alan Churley <alan.churley@gmail.com>
2014-07-14 15:47:39 +0100

    Passing results to callback

--- checker.js
+++ checker.js
@@ -1,28 +1,38 @@
-var urlChecker = {
-  checkUrl: function (url, port, path) {
-    var options = {
-        host: url,
-        port: port,
-        path: path
-      },
-      start = new Date();
+var http = require('http'),
+  urlChecker = {
+    checkUrl: function (url, port, path, callback) {
+      console.log('here');
+      var options = {
+          host: url,
+          port: port,
+          path: path
+        },
+        start = new Date();
 
-    http.get(options, function(res) {
-      var end = new Date(),
-          diff = end.getTime() - start.getTime();
-
-      return {
-        status: 1,
-        time: diff
-      };
-    }).on('error', function(e) {
-      return {
-        status: 0,
-        time: 0
-      };
-    });
-  }
-};
+      http.get(options, function(res) {
+        var end = new Date(),
+            diff = end.getTime() - start.getTime();
+        if (callback) {
+          callback(
+            {
+              status: 1,
+              time: diff
+            }
+          );
+        }
+      }).on('error', function(e) {
+        console.log('error', e);
+        if (callback) {
+          callback(
+            {
+              status: 0,
+              time: 0
+            }
+          );
+        }
+      });
+    }
+  };
 module.exports = function() {
   return urlChecker;
 };
