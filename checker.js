a104069f29add96ddc13157fa9e5d02292dcb624
Alan Churley <alan.churley@gmail.com>
2014-07-14 18:13:33 +0100

    Adding loop functionality to write to a csv file

--- checker.js
+++ checker.js
@@ -1,5 +1,46 @@
 var http = require('http'),
   urlChecker = {
+    checkLoop: function (url, port, path, outputFile) {
+      var stream = require('stream'),
+        fs = require('fs'),
+        responseStream = new stream.Readable({objectMode:true}),
+        urlChecker = require('../urlChecker/checker.js');
+
+      if (!outputFile) {
+        outputFile = new Date().getTime() + '.csv';
+      }
+
+
+      fs.writeFile(outputFile, '', function(err) {
+        if(err && err !== null) {
+              console.log(err);
+            }
+      });
+      responseStream._write = function(data,end,done) {
+        this.push(data);
+        done();
+      };
+      responseStream._read = function(){};
+
+      responseStream.on('data', function(response) {
+          var stringToAppend = response.time + ',';
+          fs.appendFile(outputFile, stringToAppend, function (err) {
+            if(err && err !== null) {
+              console.log(err);
+            }
+          });
+      });
+      this.runStreamCheck(url, port, path, responseStream);
+        
+    },
+    runStreamCheck: function (url, port, path, stream) {
+      var _this = this;
+      urlChecker.checkUrl(url, port, path, function (object) {
+        stream.push(object);
+        _this.runStreamCheck(url, port, path, stream);
+        return;
+      });
+    },
     checkUrl: function (url, port, path, callback) {
       var options = {
           host: url,
@@ -21,15 +62,17 @@ var http = require('http'),
             }
           );
         }
+        res.resume();
       }).on('error', function(e) {
         if (callback) {
           callback(
             {
               status: 0,
-              request: options
+              time: 0
             }
           );
         }
+        res.resume();
       });
     }
   };
